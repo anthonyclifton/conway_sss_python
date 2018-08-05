7f712ce3b539bab12b2e3d5ef2c915e49fa4da81
Anthony Clifton <anthony@changedearth.com>
2018-08-05 17:52:58 -0500

    Keep the live cells in a set instead of a list so there are no duplicates

--- grid.py
+++ grid.py
@@ -1,6 +1,9 @@
 class Grid(object):
     def __init__(self):
-        self.cells = []
+        self.cells = set()
 
     def birth_cell(self, coordinates):
-        self.cells.append(coordinates)
+        self.cells.add(coordinates)
+
+    def kill_cell(self, coordinates):
+        self.cells.remove(coordinates)
--- test/test_grid.py
+++ test/test_grid.py
@@ -7,7 +7,21 @@ class TestGrid(unittest.TestCase):
     def setUp(self):
         self.grid = Grid()
 
-    def test__grid__should_birth_a_cell(self):
+    def test__grid__should_birth_cell(self):
         expected_cell = (0, 0)
         self.grid.birth_cell(expected_cell)
-        self.assertEquals(self.grid.cells[0], expected_cell)
+        assert expected_cell in self.grid.cells
+
+    def test__grid__should_not_birth_two_cells_at_the_same_location(self):
+        cell_to_birth = (0, 0)
+        self.grid.birth_cell(cell_to_birth)
+        self.grid.birth_cell(cell_to_birth)
+        self.assertEquals(len(self.grid.cells), 1)
+
+    def test__grid__should_kill_cell(self):
+        cell_to_kill = (0, 0)
+        self.grid.birth_cell(cell_to_kill)
+        assert cell_to_kill in self.grid.cells
+
+        self.grid.kill_cell(cell_to_kill)
+        assert cell_to_kill not in self.grid.cells
