5dabff569565307cbe99ce1f52a1b79cdf732523
Anthony Clifton <anthony@changedearth.com>
2018-08-05 22:42:53 -0500

    Added a cleanup method to screen service

--- screen_service.py
+++ screen_service.py
@@ -1,11 +1,19 @@
 
 class ScreenService(object):
     def __init__(self, curses):
+        self.curses = curses
         self.stdscr = curses.initscr()
         curses.noecho()
         curses.cbreak()
+        self.stdscr.keypad(1)
         self.screen = self.stdscr.subwin(23, 79, 0, 0)
 
     def draw_border(self):
         self.screen.box()
         self.screen.refresh()
+
+    def cleanup(self):
+        self.stdscr.keypad(0)
+        self.curses.echo()
+        self.curses.nocbreak()
+        self.curses.endwin()
--- test/test_screen_service.py
+++ test/test_screen_service.py
@@ -1,6 +1,6 @@
 import unittest
 
-from mock import MagicMock
+from mock import MagicMock, call
 
 from screen_service import ScreenService
 
@@ -22,8 +22,19 @@ class TestScreenService(unittest.TestCase):
         self.mock_curses.noecho.assert_called_once()
         self.mock_curses.cbreak.assert_called_once()
         self.mock_stdscn.subwin.assert_called_once()
+        self.mock_stdscn.keypad.assert_called_once_with(1)
 
     def test__draw_border__should_draw_border(self):
         self.screen_service.draw_border()
+
         self.mock_screen.box.assert_called_once()
         self.mock_screen.refresh.assert_called_once()
+
+    def test__cleanup__should_return_terminal_to_sane_state(self):
+        self.screen_service.cleanup()
+
+        calls = [call(1), call(0)]
+        self.mock_stdscn.keypad.assert_has_calls(calls)
+        self.mock_curses.echo.assert_called_once()
+        self.mock_curses.nocbreak.assert_called_once()
+        self.mock_curses.endwin.assert_called_once()
