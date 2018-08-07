7674b0718e3bdf390ec4382ec4dc1e164d86c3b0
Anthony Clifton <anthony@changedearth.com>
2018-08-06 22:56:51 -0500

    Add empty game and test

---
+++ test/test_game.py
@@ -0,0 +1,10 @@
+import unittest
+from game import Game
+
+
+class TestGame(unittest.TestCase):
+    def setUp(self):
+        self.game = Game()
+
+    def test__something__should_do_something(self):
+        pass
