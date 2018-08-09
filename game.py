43a45dbae1e645c331eea6df0dfe276006e8a17e
Anthony Clifton <anthony@changedearth.com>
2018-08-08 20:49:48 -0500

    Added game update method that updates with a random cell, removing the previous cell

--- game.py
+++ game.py
@@ -1,3 +1,5 @@
+from random import randint
+
 from grid import Grid
 
 
@@ -5,3 +7,9 @@ class Game(object):
     def __init__(self, screen_service):
         self.grid = Grid()
         self.screen_service = screen_service
+
+    def update(self):
+        if len(self.grid.cells) > 0:
+            self.grid.kill_cell(list(self.grid.cells)[0])
+        random_cell = (randint(0, 9), randint(0, 9))
+        self.grid.birth_cell(random_cell)
--- test/test_game.py
+++ test/test_game.py
@@ -14,3 +14,16 @@ class TestGame(unittest.TestCase):
 
     def test__init__should_store_screen_service(self):
         self.assertEquals(self.game.screen_service, self.mock_screen_service)
+
+    def test__update__should_add_new_random_cell(self):
+        self.assertEquals(len(self.game.grid.cells), 0)
+        self.game.update()
+        self.assertEquals(len(self.game.grid.cells), 1)
+
+    def test__update__should_remove_old_cell_when_adding_new(self):
+        self.game.update()
+        first_cell = list(self.game.grid.cells)[0]
+        self.game.update()
+        assert first_cell not in self.game.grid.cells
+        self.assertEquals(len(self.game.grid.cells), 1)
+
