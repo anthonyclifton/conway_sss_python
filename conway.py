81d7cc21a7eeca720d772fa6135bbe4a2b3add78
Anthony Clifton <anthony@changedearth.com>
2018-08-14 00:15:09 -0500

    Grid should be injected from edge to facilitate testing

--- conway.py
+++ conway.py
@@ -1,10 +1,12 @@
 import curses
 
 from game import Game
+from grid import Grid
 from screen_service import ScreenService
 
 screen_service = ScreenService(curses)
-game = Game(screen_service)
+grid = Grid()
+game = Game(screen_service, grid)
 
 if __name__ == '__main__':
     game.start()
